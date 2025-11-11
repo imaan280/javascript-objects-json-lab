32c708331296f456ce394f6b6ff2c47fdafa00c4
c4021820 <c4021820@my.shu.ac.uk>
2025-11-11 09:43:24 +0000

    displaying time

--- js/clock.js
+++ js/clock.js
@@ -1,3 +1,11 @@
 (function(){ 
-    console.info("Hello JS"); 
+     
+    const myNode = document.createElement("div");
+    const digitalClock = document.getElementById("digitalClock")
+    digitalClock.appendChild(myNode);
+    const myDate =  new Date();
+    console.dir(myDate)
+    // show only hours, minutes and seconds
+    myNode.innerHTML = myDate.toLocaleTimeString("en-GB", { timeStyle: "medium" });
+
 })();
