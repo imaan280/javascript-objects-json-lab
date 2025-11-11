d896ad0c8d156aa7042ab1f1a97a3f8075fa2377
c4021820 <c4021820@my.shu.ac.uk>
2025-11-11 10:45:57 +0000

    staff.js update

--- js/staff.js
+++ js/staff.js
@@ -1,19 +1,20 @@
-var qualifications = [
-    {
-       subject : "Maths",
-       grade : "A"
-    },
-    {
-       subject : "English",
-       grade : "B"
-    }, 
-    {
-       subject : "French",
-       grade : "C"
-    },
-    {
-       subject : "Physics",
-       grade : "B"
-    
-    }
-    ];
+(() => {
+
+    const staffTable = document.getElementById("staffTable");
+
+    fetch("data/staff.json")
+  .then((response) => response.json())
+  .then((data) => {
+    console.dir(data)
+    data.forEach((item) => {
+        const row = document.createElement("tr");
+        const nameCell = document.createElement("td");
+        const emailCell = document.createElement("td");
+        nameCell.textContent = `${item.first_name}  ${item.last_name}`;
+        emailCell.textContent = item.email;
+        row.appendChild(nameCell);
+        row.appendChild(emailCell);
+        staffTable.appendChild(row);
+      });
+})
+    })();
