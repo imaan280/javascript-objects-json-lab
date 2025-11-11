6f71f29508d94b38f7fd93be3047c17cb8db364d
c4021820 <c4021820@my.shu.ac.uk>
2025-11-11 10:30:07 +0000

    staff.js

---
+++ js/staff.js
@@ -0,0 +1,19 @@
+var qualifications = [
+    {
+       subject : "Maths",
+       grade : "A"
+    },
+    {
+       subject : "English",
+       grade : "B"
+    }, 
+    {
+       subject : "French",
+       grade : "C"
+    },
+    {
+       subject : "Physics",
+       grade : "B"
+    
+    }
+    ];
