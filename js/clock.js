9b2153fc8123021b0d540adab3a4673d3f37db54
c4021820 <c4021820@my.shu.ac.uk>
2025-11-11 09:47:58 +0000

    make the clock live

--- js/clock.js
+++ js/clock.js
@@ -3,9 +3,13 @@
     const myNode = document.createElement("div");
     const digitalClock = document.getElementById("digitalClock")
     digitalClock.appendChild(myNode);
-    const myDate =  new Date();
-    console.dir(myDate)
-    // show only hours, minutes and seconds
-    myNode.innerHTML = myDate.toLocaleTimeString("en-GB", { timeStyle: "medium" });
-
+    const updateTime = () => {
+        const myDate =  new Date();
+        console.dir(myDate)
+        myNode.innerHTML = myDate.toLocaleTimeString("en-GB", { timeStyle: "medium" });
+        console.info('tick tock');
+        };
+        
+    setInterval(updateTime, 1000);
+    updateTime();
 })();
